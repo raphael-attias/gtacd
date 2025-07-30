9c45b0d671207ea1f7679e4c3886374030088270
raphael-attias <137683618+raphael-attias@users.noreply.github.com>
2025-07-30 07:40:23 +0200

    Ajout de l'affichage des secrets + simulation d’échec

--- app/check_app.sh
+++ app/check_app.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
 echo "[INFO] Début de la vérification de l'application..."
-
-FILE="index.html"
-
-if [ -f "$FILE" ]; then
-    echo "[SUCCESS] Le fichier $FILE existe."
-    echo "[INFO] Vérification terminée avec succès."
-    exit 0
-else
-    echo "[ERROR] Le fichier $FILE est manquant !"
-    echo "[INFO] Vérification échouée."
-    exit 1
-fi
+exit 1
