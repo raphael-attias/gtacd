aa186505166bab92367a76795b4c28d738b36d13
raphael-attias <137683618+raphael-attias@users.noreply.github.com>
2025-07-31 21:39:29 +0200

    retest-f

--- app/check_app.sh
+++ app/check_app.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-echo "[INFO] Début de la vérification de l'application..."
-exit 1
+echo "Début des vérifications..."
+
+if [ -f "app/index.html" ]; then
+    echo "Fichier index.html trouvé."
+    echo "Fin des vérifications avec succès."
+    exit 0
+else
+    echo "Fichier index.html manquant."
+    echo "Fin des vérifications avec erreur."
+    exit 1
+fi
